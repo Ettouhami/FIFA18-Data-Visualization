d79879fc18c5b56cf74139094a5da34313443936
Ansaf Ahmad <ansafahmad70@gmail.com>
2017-12-01 17:02:57 -0500

    Added map and markers

--- server.R
+++ server.R
@@ -7,6 +7,7 @@
 
 library(shiny)
 library(radarchart)
+library(leaflet)
 shinyServer(function(input, output) {
 
   output$distPlot <- renderPlot({
@@ -24,18 +25,145 @@ shinyServer(function(input, output) {
     counts <- table(mtcars$gear)
     barplot(counts, main = "Car Mpg", xlab="Gears", horiz=TRUE, names.arg = c("3 Gears","4 Gears", "5 Gears"))
   })
+  
+
+
+  output$value <- renderPrint({ 
+    if(input$radio == 1){1}
+    else if(input$radio == 2){2}
+    
+    })
+  
+  
 
   output$playerChart <- renderChartJSRadar({
-    scores <- data.frame("Label"=c("Communicator", "Data Wangler", "Programmer",
-                                   "Technologist",  "Modeller", "Visualizer"),
-                         "Rich" = c(9, 7, 4, 5, 3, 7),
-                         "Andy" = c(7, 6, 6, 2, 6, 9),
-                         "Aimee" = c(6, 5, 8, 4, 7, 6))
+    label <- c("Free Kick",
+    "Acceleration",
+    "Aggression",
+    "Agility",
+    "Balance",
+    "Ball Control",
+    "Dribbling",
+    "Finishing")
     
-    chartJSRadar(scores, maxScale = 10, showToolTipLabel=TRUE)
+    higuain <- c(62,
+                 78,
+                 50,
+                 75,
+                 69,
+                 85,
+                 84,
+                 91
+    )
+    
+    kroos <- c(84,
+              60,
+              60,
+              71,
+              69,
+              89,
+              79,
+              76
+    )
+    
+    hazard <- c(79,
+               93,
+               54,
+               93,
+               91,
+               92,
+               93,
+               83
+    )
+    
+    ramos <- c(67,
+               75,
+               84,
+               79,
+               60,
+               84,
+               61,
+               60
+    )
+    
+    lewandowski <- c(84,
+                     79,
+                     80,
+                     78,
+                     80,
+                     89,
+                     85,
+                     91
+    )
+    
+    suarez <- c(84,
+                     88,
+                     78,
+                     86,
+                     60,
+                     91,
+                     86,
+                     94
+    )
+    
+    ronaldo <- c(76,
+                 89,
+                 63,
+                 89,
+                 63,
+                 93,
+                 91,
+                 94
+    )
+    
+    messi <- c(90,
+               92,
+               48,
+               90,
+               95,
+               95,
+               97,
+               95
+    )
+    
+    neymar <- c(84,
+                94,
+                56,
+                96,
+                82,
+                95,
+                96,
+                89
+    )
+    
+    
+    scores <- data.frame("Label"=label,
+                         "Eden Hazard" = hazard,
+                         "Cristiano Ronaldo" = ronaldo,
+                         "Neymar" = neymar,
+                         "Lionel Messi" = messi,
+                         "Luis Suarez" = suarez,
+                         "Robert Lewandowski" = lewandowski, 
+                         "Sergio Ramos" = ramos,
+                         "Gonzalo Higuain" = higuain,
+                         "Toni Kroos" = kroos)
+    
+    chartJSRadar(scores, maxScale = 100, showToolTipLabel=TRUE)
     
   })
   
-
+  
+ output$map <- renderLeaflet({
+   Location <- c("Atlanta ","Los Angeles","Chicago","New York","Dallas","Baltimore","Phoenix","Charlotte","Houston","San Antonio", "Seattle" )
+   
+   #### Latitude and Longitude values for each of the above location
+   Lat <- c(33.74401,33.82377,41.78798,40.767309,32.88153,39.148492,33.45444,35.2406,29.935842,29.44838,47.714965 )
+   Lon <- c(-84.56032,-118.2668,-87.7738,-73.978308,-96.64601,-76.796211,-112.32401,-81.04028,-95.398436,-98.39908,-122.127166 )
+   df <- data.frame(Location, Lat,Lon)
+   leaflet()%>%
+     addTiles()%>%
+     setView(lng=-90, lat=45, zoom=2)%>%
+     addMarkers(as.vector(df[1:11,"Lon"]),as.vector(df[1:11,"Lat"]))
+ })
   
 })
